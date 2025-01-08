5dfd471fd4abbde62df1f65e2ff06cc6ed40f9c7
Ignacio Flores <39257795+ignacio-flores@users.noreply.github.com>
2025-01-08 10:47:26 +0100

    externalize more features to yaml config

--- app.R
+++ app.R
@@ -6,19 +6,19 @@ load_config("tests/config.yaml")
 
 #create visualization nº1
 createViz(
-  data.file = file.path("data/ineq_warehouse_meta_v1_2.csv"),
-  new.cols = combine_col,
-  data.wrangler = "tests/dictionary_loader_ineq.R",
-  x_var = "year",
-  y_var = "value",
-  color_var = "viz_color_var",
+  data.file = data.file,
+  data.wrangler = data.wrangler,
+  new.cols = new.cols,
+  gopts = gopts, 
+  x_var = mainvars$x.var,
+  y_var = mainvars$y.var,
+  color_var = mainvars$color.var,
   selector_info = fixed_sel,
   loose_selectors = reactive_sel,
   tooltip_vars = tooltip,
   table.display = T,
   dt.cols = dt_cols,
-  download.button = T,
-  gopts = c("line", "point")
+  download.button = T
   #hide.selectors = T,
   #listen = T,
 )
