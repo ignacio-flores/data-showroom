6737a2f556b8de4110843bcdea328f92e83e6521
Ignacio Flores <39257795+ignacio-flores@users.noreply.github.com>
2026-04-24 10:13:11 +0200

    deliver first inhe draft

--- deploy-app.R
+++ deploy-app.R
@@ -1,9 +1,9 @@
 library(rsconnect)
-source("auth/shiny_auth_frolle.R")
+source("auth/shiny_auth_gregcull.R")
 rsconnect::setAccountInfo(name = shiny_account,
                           token = shiny_token,
                           secret = shiny_secret)
 
 # Define name
-name = "inhe_prev"
+name = "inhe_multi"
 deployApp(appDir = ".", appName = name)
